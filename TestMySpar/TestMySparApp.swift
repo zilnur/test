2062d41b0b9f1917253820e1d333c137267d974b
zilnur <86298777+zilnur@users.noreply.github.com>
2023-08-10 14:45:08 +0300

    Initial Commit

---
+++ TestMySpar/TestMySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMySparApp.swift
+//  TestMySpar
+//
+//  Created by Ильнур Закиров on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestMySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
